59f5d22849c157c2c3478208eda474870c75044d
gregorie536 <vexier.gregorie@gmail.com>
2023-10-05 14:14:15 +0200

    VIEWS : création des composants navbar et footer

---
+++ resources/js/Components/Footer.vue
@@ -0,0 +1,85 @@
+<template>
+    <nav class="footer">
+        <div class="footer-container container">
+            <ul class="menu-items">
+                <li>Copyright &copy; | Tous droits réservés</li>
+                <li class="font-bold">
+                    <router-link to="/#">Mentions légales</router-link>
+                </li>
+                <li>
+                    <small>
+                        Le contenu du présent site web doit être utilisé à des
+                        fins informatives et non-commerciales
+                    </small>
+                </li>
+            </ul>
+
+            <router-link to="/">
+                <!-- <h1 class="logo">
+          <img :src="logoHeader.url" :alt="logoHeader.alt" />
+        </h1> -->
+            </router-link>
+        </div>
+    </nav>
+</template>
+<script>
+export default {
+    name: "Footer",
+    // data() {
+    //   return {
+    //     logoFooter: {
+    //       url: require("@/assets/img/logo/logo.svg"),
+    //       alt: "logo",
+    //     },
+    //   };
+    // },
+};
+</script>
+
+<style scoped lang="scss">
+@import "../../css/variables.scss";
+/****************************************************
+                      FOOTER
+*****************************************************/
+
+// .logoBas {
+//   width: 100px;
+//   padding-top: 10px;
+// }
+
+// footer {
+//   text-align: center;
+//   margin-right: 20px;
+//   margin-left: 20px;
+//   border-top: 1px solid var(--black);
+// }
+
+// .liens {
+//   display: flex;
+//   flex-direction: column;
+//   justify-content: space-between;
+//   .pageFooter {
+//     font-size: 15px;
+//   }
+// }
+.footer {
+    background-color: $main-text-color;
+    color: $nav-bg-color;
+    .footer-container {
+        display: flex;
+        justify-content: center;
+        // flex-direction: column;
+    }
+    .menu-items{
+      display: flex;
+        // justify-content: center;
+        flex-direction: column;
+        // text-align: center;
+        align-items: center;
+    }
+    small {
+        display: flex;
+        text-align: center;
+    }
+}
+</style>
---
+++ resources/js/Components/Navbar.vue
@@ -0,0 +1,231 @@
+<template>
+    <nav class="navbar">
+        <div class="navbar-container container">
+            <input type="checkbox" name="" id="" />
+            <div class="hamburger-lines">
+                <span class="line line1"></span>
+                <span class="line line2"></span>
+                <span class="line line3"></span>
+            </div>
+            <ul class="menu-items">
+                <li><router-link to="/acceuil">Accueil</router-link></li>
+                <li>
+                    <router-link to="/event">Sorties et séjours</router-link>
+                </li>
+                <li>
+                    <router-link to="/organization">Association</router-link>
+                </li>
+                <li>
+                    <router-link to="/guideline"
+                        >Règles et organisation</router-link
+                    >
+                </li>
+                <li>
+                    <router-link to="/gallery-photos"
+                        >Galerie photos</router-link
+                    >
+                </li>
+                <li><router-link to="/contact">Contact</router-link></li>
+            </ul>
+            <router-link to="/">
+                <!-- <h1 class="logo">
+            <img :src="logoHeader.url" :alt="logoHeader.alt" />
+          </h1> -->
+            </router-link>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: "NavBar",
+
+    // data() {
+    //   return {
+    //     logoHeader: {
+    //       url: require("@/assets/img/logo/logo.svg"),
+    //       alt: "logo",
+    //     },
+    //   };
+    // },
+    methods: {
+        sidemenu: {
+            el: "#sidemenu",
+            data: {
+                navOpen: false,
+            },
+        },
+    },
+};
+</script>
+
+<style scoped lang="scss">
+@import "../../css/variables.scss";
+
+.navbar {
+    background-color: $main-text-color;
+    color: $nav-bg-color;
+}
+.navbar input[type="checkbox"],
+.navbar .hamburger-lines {
+    display: none;
+}
+
+.container {
+    max-width: 1200px;
+    width: 90%;
+    margin: auto;
+}
+
+.navbar-container {
+    display: flex;
+    justify-content: space-between;
+    height: 64px;
+    align-items: center;
+}
+
+.menu-items {
+    order: 2;
+    display: flex;
+}
+.logo {
+    order: 1;
+    font-size: 2.3rem;
+    width: 130px;
+}
+
+.menu-items li {
+    list-style: none;
+    margin-left: 1.5rem;
+    font-size: 1.3rem;
+}
+
+//   .navbar a {
+//     color: $nav-bg-color;
+//     // text-decoration: none;
+//     // font-weight: 500;
+//     // transition: color 0.3s ease-in-out;
+//   }
+
+//   .navbar a:hover {
+//     color: var(--couleur-burlywood);
+//   }
+
+@media (max-width: 768px) {
+    .navbar {
+        opacity: 0.95;
+    }
+
+    .navbar-container input[type="checkbox"],
+    .navbar-container .hamburger-lines {
+        display: block;
+    }
+
+    .navbar-container {
+        display: block;
+        position: relative;
+        height: 64px;
+    }
+
+    .navbar-container input[type="checkbox"] {
+        position: absolute;
+        display: block;
+        height: 32px;
+        width: 30px;
+        top: 20px;
+        left: 20px;
+        z-index: 5;
+        opacity: 0;
+        cursor: pointer;
+        right: 20px;
+        left: auto;
+    }
+
+    .navbar-container .hamburger-lines {
+        display: block;
+        height: 28px;
+        width: 35px;
+        position: absolute;
+        top: 20px;
+        left: 20px;
+        z-index: 2;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        right: 20px;
+        left: auto;
+    }
+
+    .navbar-container .hamburger-lines .line {
+        display: block;
+        height: 4px;
+        width: 100%;
+        border-radius: 10px;
+        background: $nav-bg-color;
+    }
+
+    .navbar-container .hamburger-lines .line1 {
+        transform-origin: 0% 0%;
+        transition: transform 0.3s ease-in-out;
+    }
+
+    .navbar-container .hamburger-lines .line2 {
+        transition: transform 0.2s ease-in-out;
+    }
+
+    .navbar-container .hamburger-lines .line3 {
+        transform-origin: 0% 100%;
+        transition: transform 0.3s ease-in-out;
+    }
+
+    .navbar .menu-items {
+        padding-top: 100px;
+        background: $main-text-color;
+        height: 100vh;
+        max-width: 300px;
+        transform: translate(-150%);
+        display: flex;
+        flex-direction: column;
+        margin-left: -40px;
+        padding-left: 40px;
+        transition: transform 0.5s ease-in-out;
+        //   box-shadow: 5px 0px 10px 0px #aaa;
+        overflow: scroll;
+    }
+
+    .navbar .menu-items li {
+        margin-bottom: 1.8rem;
+        font-size: 1.1rem;
+        font-weight: 500;
+    }
+
+    .logo {
+        position: absolute;
+        top: 10px;
+        right: 15px;
+        font-size: 2.5rem;
+    }
+
+    .navbar-container input[type="checkbox"]:checked ~ .menu-items {
+        transform: translateX(0);
+    }
+
+    .navbar-container input[type="checkbox"]:checked ~ .hamburger-lines .line1 {
+        transform: rotate(45deg);
+    }
+
+    .navbar-container input[type="checkbox"]:checked ~ .hamburger-lines .line2 {
+        transform: scaleY(0);
+    }
+
+    .navbar-container input[type="checkbox"]:checked ~ .hamburger-lines .line3 {
+        transform: rotate(-45deg);
+    }
+}
+
+@media (max-width: 500px) {
+    .navbar-container input[type="checkbox"]:checked ~ .logo {
+        display: none;
+    }
+}
+</style>
