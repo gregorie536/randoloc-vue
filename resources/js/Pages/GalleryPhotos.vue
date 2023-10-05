64827e1f4bf2b178b6c440983aadfa16c9482ce0
gregorie536 <vexier.gregorie@gmail.com>
2023-10-05 16:45:12 +0200

    VIEWS : mise en place de toutes les pages

---
+++ resources/js/Pages/GalleryPhotos.vue
@@ -0,0 +1,30 @@
+
+<template>
+    <MainLayout>
+        <div class="flex justify-center items-center">
+            <div>
+                <h1>{{ titre }}</h1>
+            </div>
+        </div>
+        
+    </MainLayout>
+</template>
+
+<script>
+import MainLayout from '@/Layouts/MainLayout.vue';
+export default {
+    components: {
+        MainLayout,
+    },
+    name: "GalleryPhotos",
+    data() {
+        return {
+            titre: "Galerie photos",
+        };
+    },
+};
+</script>
+
+<style scoped lang="scss">
+@import "../../css/style.scss";
+</style>
---
+++ resources/js/Pages/Guideline.vue
@@ -0,0 +1,34 @@
+
+<template>
+    <MainLayout>
+        <div class="flex justify-center items-center">
+            <div>
+                <h1>{{ titre }}</h1>
+            </div>
+        </div>
+        <div class="flex justify-center items-center">
+            <div class="content">
+                
+            </div>
+        </div>
+    </MainLayout>
+</template>
+
+<script>
+import MainLayout from '@/Layouts/MainLayout.vue';
+export default {
+    components: {
+        MainLayout,
+    },
+    name: "Guideline",
+    data() {
+        return {
+            titre: "Règles et organisations",
+        };
+    },
+};
+</script>
+
+<style scoped lang="scss">
+@import "../../css/style.scss";
+</style>
