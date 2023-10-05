a750c872999b35ec45513d51f137e6b491e93e33
gregorie536 <vexier.gregorie@gmail.com>
2023-10-05 14:12:15 +0200

    VIEWS : layout modifiée pour navbar et footer

---
+++ resources/js/Layouts/MainLayout.vue
@@ -0,0 +1,34 @@
+<!-- resources/js/Layouts/MainLayout.vue -->
+<template>
+  <div class="layout-container">
+    <Navbar />
+    <main class="main-content">
+      <slot></slot> <!-- Le contenu de la page sera injecté ici -->
+    </main>
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Navbar from '@/Components/Navbar.vue'
+import Footer from '@/Components/Footer.vue'
+
+export default {
+components: {
+  Navbar,
+  Footer
+}
+}
+</script>
+
+<style lang="scss">
+.layout-container {
+display: flex;
+flex-direction: column;
+min-height: 100vh; // Utilisez vh pour s'assurer que cela prend toute la hauteur de la fenêtre de visualisation
+
+.main-content {
+  flex: 1; // Prend tout l'espace disponible
+}
+}
+</style>
