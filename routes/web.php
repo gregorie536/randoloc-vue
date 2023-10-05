6f36cab748c7bc663b31a63b407c3da807fde003
gregorie536 <vexier.gregorie@gmail.com>
2023-10-05 07:55:58 +0200

    ROUTES : mise en place des différentes routes du projet

--- routes/web.php
+++ routes/web.php
@@ -25,14 +25,44 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return Inertia::render('Dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+// Route::middleware('auth')->group(function () {
+//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// });
+
+// Route pour la page d'acceuil
+Route::get('/acceuil', function () {
+    return Inertia::render('Acceuil');
+})->name('acceuil');
+
+// Route pour la page evenements
+Route::get('/event', function () {
+    return Inertia::render('Event');
+})->name('event');
+
+// Route pour la page de la galerie photos
+Route::get('/gallery-photos', function () {
+    return Inertia::render('GalleryPhotos');
+})->name('galleryPhotos');
+
+// Route pour la page organisation
+Route::get('/organization', function () {
+    return Inertia::render('Organization');
+})->name('organization');
+
+// Route pour la page règlement et organisation
+Route::get('/guideline', function () {
+    return Inertia::render('Guideline');
+})->name('guideline');
+
+// Route pour la page contact
+Route::get('/contact', function () {
+    return Inertia::render('Contact');
+})->name('contact');
 
 require __DIR__.'/auth.php';
