330de5302741e08bb4bdbe10c9763e269b291091
gregorie536 <vexier.gregorie@gmail.com>
2023-10-26 17:42:11 +0200

    VIEW - mise en place del a vue Contact.vue

--- resources/js/Pages/Contact.vue
+++ resources/js/Pages/Contact.vue
@@ -8,21 +8,31 @@
         <div class="flex justify-center items-center">
             <div class="content">
                 <p class="font-bold">Présidente de l'association :</p>
-                <ul>
-                    <li>{{ contact.name }}</li>
-                    <li>{{ contact.phone_number }}</li>
-                    <li>{{ contact.email }}</li>
-                </ul>
-                <!-- <p class="font-bold">Adresse du site de l'association :</p>
-                <ul>
-                    <li>À définir</li>
-                </ul>
-                <p class="font-bold">Contact pour le site :</p>
-                <ul>
+                <!-- <ul>
                     <li>{{ contact.name }}</li>
                     <li>{{ contact.phone_number }}</li>
                     <li>{{ contact.email }}</li>
                 </ul> -->
+                <ul>
+                    <li>{{ presidentContact.name }}</li>
+                    <li>{{ presidentContact.phone_number }}</li>
+                    <li>{{ presidentContact.email }}</li>
+                </ul>
+                <p class="font-bold">Adresse du site de l'association :</p>
+                <ul>
+                    <li>À définir</li>
+                </ul>
+                <p class="font-bold">Contact pour le site :</p>
+                <!-- <ul>
+                    <li>{{ contact.name }}</li>
+                    <li>{{ contact.phone_number }}</li>
+                    <li>{{ contact.email }}</li>
+                </ul> -->
+                <ul>
+                    <li>{{ siteContact.name }}</li>
+                    <li>{{ siteContact.phone_number }}</li>
+                    <li>{{ siteContact.email }}</li>
+                </ul>
                 <!-- <div>{{ contact }}</div> -->
             </div>
         </div>
@@ -38,7 +48,9 @@ export default {
     },
     name: "Contact",
     props: {
-        contact: Object,
+        // contact: Object,
+        presidentContact: Object,
+        siteContact: Object,
     },
     data() {
         return {
