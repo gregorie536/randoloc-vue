82d46aa7df99992b4d46e9839791e7d937cddab7
gregorie536 <vexier.gregorie@gmail.com>
2023-10-26 17:48:15 +0200

    VIEW - mise en place de la vue d'édition de la page guideline

---
+++ resources/js/Pages/Guideline/Edit.vue
@@ -0,0 +1,101 @@
+<template>
+    <div class="bg-main-bg-color p-8 rounded-md shadow-lg">
+        <form @submit.prevent="submitForm" class="space-y-6">
+            <div
+                v-for="(guideline, index) in guidelines"
+                :key="guideline.id"
+                class="border-b-2 border-aliceblue pb-6 mb-6"
+            >
+                <h2 class="text-xl font-semibold text-main-text-color mb-4">
+                    Prix {{ index + 1 }} : {{ guideline.type }}
+                </h2>
+                <div class="flex flex-col mb-4">
+                    <!-- <label
+                        :for="'price' + index"
+                        class="text-main-text-color mb-1"
+                        >Prix :</label
+                    > -->
+                    <input
+                        :id="'price' + index"
+                        v-model="guideline.price"
+                        class="p-2 rounded-md border border-aliceblue focus:outline-none focus:border-nav-bg-color"
+                    />
+                </div>
+            </div>
+
+            <div class="flex flex-col mb-4">
+                <h2 class="text-xl font-semibold text-main-text-color mb-4">
+                    Année de saison :
+                </h2>
+                <input
+                    id="season_year"
+                    v-model="seasonYear"
+                    class="p-2 rounded-md border border-aliceblue focus:outline-none focus:border-nav-bg-color"
+                />
+            </div>
+
+            <button
+                type="submit"
+                class="bg-nav-bg-color text-white py-2 px-6 rounded-md hover:bg-opacity-90 focus:outline-none"
+            >
+                Mettre à jour
+            </button>
+        </form>
+    </div>
+</template>
+
+<script>
+import { ref } from "vue";
+import { Inertia } from "@inertiajs/inertia";
+
+export default {
+    props: {
+        guidelines: Array,
+        seasonYear: Date,
+    },
+
+    setup(props) {
+        const guidelines = ref(props.guidelines);
+        const seasonYear = ref(props.seasonYear);
+
+        console.log("Initial seasonYear:", props.seasonYear);
+
+        function submitForm() {
+            const updatedGuidelines = guidelines.value.map((guideline) => {
+                return {
+                    id: guideline.id,
+                    type: guideline.type,
+                    price: guideline.price,
+                    season_year: seasonYear.value,
+                };
+            });
+            Inertia.post("/guidelines/update", {
+                guidelines: updatedGuidelines,
+            });
+        }
+
+        return {
+            submitForm,
+            seasonYear,
+        };
+    },
+};
+</script>
+
+<style scoped>
+.bg-main-bg-color {
+    background-color: rgba(200, 173, 127, 0.7);
+}
+
+.text-main-text-color {
+    color: #223663;
+}
+
+.border-aliceblue {
+    border-color: #f0f8ff;
+}
+
+.bg-nav-bg-color {
+    background-color: #c8ad7f;
+}
+</style>
