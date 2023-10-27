1627f7d9d8fb5c3245df95a2b80c0ffbb6948ec1
gregorie536 <vexier.gregorie@gmail.com>
2023-10-27 21:01:51 +0200

    VIEW - mise en place de la page GalleryPhotos

--- resources/js/Pages/GalleryPhotos.vue
+++ resources/js/Pages/GalleryPhotos.vue
@@ -1,4 +1,3 @@
-
 <template>
     <MainLayout>
         <div class="flex justify-center items-center">
@@ -6,17 +5,34 @@
                 <h1>{{ titre }}</h1>
             </div>
         </div>
-        
+        <div class="flex flex-wrap">
+            <div
+                class="max-w-sm rounded-card overflow-hidden shadow-lg m-2"
+                v-for="image in galleries"
+                :key="image.id"
+            >
+                <div class="image-container">
+                    <img
+                        :src="'/storage/' + image.image"
+                        :alt="image.comment"
+                    />
+                </div>
+            </div>
+        </div>
     </MainLayout>
 </template>
 
 <script>
-import MainLayout from '@/Layouts/MainLayout.vue';
+import MainLayout from "@/Layouts/MainLayout.vue";
+
 export default {
     components: {
         MainLayout,
     },
     name: "GalleryPhotos",
+    props: {
+        galleries: Array,
+    },
     data() {
         return {
             titre: "Galerie photos",
@@ -27,4 +43,28 @@ export default {
 
 <style scoped lang="scss">
 @import "../../css/style.scss";
-</style>
+
+.rounded-card {
+    @include rounded-card;
+}
+
+// @media screen and (min-width: 1025px) {
+//     @include flex-and-rounded-card;
+// }
+
+// .main-text-color {
+//     color: $main-text-color;
+// }
+
+// .image-container {
+//     width: 700px; // Votre largeur souhaitée
+//     height: 200px; // Votre hauteur souhaitée
+//     // overflow: hidden; // Cache tout ce qui dépasse du conteneur
+// }
+
+// .image-container img {
+//     width: 100%;
+//     height: 100%;
+//     // object-fit: cover;
+// }
+// </style>
