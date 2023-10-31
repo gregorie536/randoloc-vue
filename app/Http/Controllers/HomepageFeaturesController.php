6808814d5b49c22124e31390bd881d12d667af69
gregorie536 <vexier.gregorie@gmail.com>
2023-10-31 14:26:01 +0100

    CONTROLLER - mise en place du controller info du moment

---
+++ app/Http/Controllers/HomepageFeaturesController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\HomepageFeature;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+
+
+class HomepageFeaturesController extends Controller
+{
+    public function index()
+    {
+        $homepageFeatures = HomepageFeature::all();
+
+        return Inertia::render('HomepageFeature', [
+            'homepageFeatures' => $homepageFeatures
+        ]);
+    }
+
+
+    public function edit()
+    {
+        $homepageFeatures = HomepageFeature::all();
+        return Inertia::render('HomepageFeature/Edit', ['homepageFeatures' => $homepageFeatures]);
+    }
+
+    public function update(Request $request)
+    {
+        foreach ($request->homepageFeatures as $updatedHomepageFeature) {
+            $homepageFeature = HomepageFeature::find($updatedHomepageFeature['id']);
+            $homepageFeature->update($updatedHomepageFeature);
+        }
+        return redirect()->route('dashboard')->with('successMessage', 'Mise à jour réussie !');
+    }
+}
