3011d4482f4ecf0ca654bf79dee7d7b42e6a4ef2
gregorie536 <vexier.gregorie@gmail.com>
2023-10-31 14:19:27 +0100

    MODELS - update

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -9,17 +9,5 @@ class Contact extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['name', 'phone_number', 'email', 'type'];
-
-    protected function formatPhoneNumber($number)
-    {
-        return preg_replace('/(\d{2})(?=\d)/', '$1 ', $number);
-    }
-
-    public function getFormattedPhoneNumberAttribute()
-    {
-        return $this->formatPhoneNumber($this->phone_number);
-    }
-
-    
+    protected $fillable = ['lastname', 'firstname', 'phone_number', 'email', 'type'];
 }
