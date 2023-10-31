012a3d09a8c3831b741550d29076ed58c53db75e
gregorie536 <vexier.gregorie@gmail.com>
2023-10-31 14:20:05 +0100

    VIEW - update

--- resources/js/Pages/Contact.vue
+++ resources/js/Pages/Contact.vue
@@ -7,14 +7,9 @@
         </div>
         <div class="flex justify-center items-center">
             <div class="content">
-                <p class="font-bold">Présidente de l'association :</p>
-                <!-- <ul>
-                    <li>{{ contact.name }}</li>
-                    <li>{{ contact.phone_number }}</li>
-                    <li>{{ contact.email }}</li>
-                </ul> -->
+                <p class="font-bold">Président(e) de l'association :</p>
                 <ul>
-                    <li>{{ presidentContact.name }}</li>
+                    <li>{{ presidentContact.firstname }} {{ presidentContact.lastname }}</li>
                     <li>{{ presidentContact.phone_number }}</li>
                     <li>{{ presidentContact.email }}</li>
                 </ul>
@@ -23,17 +18,11 @@
                     <li>À définir</li>
                 </ul>
                 <p class="font-bold">Contact pour le site :</p>
-                <!-- <ul>
-                    <li>{{ contact.name }}</li>
-                    <li>{{ contact.phone_number }}</li>
-                    <li>{{ contact.email }}</li>
-                </ul> -->
                 <ul>
-                    <li>{{ siteContact.name }}</li>
+                    <li>{{ siteContact.firstname }} {{ siteContact.lastname }}</li>
                     <li>{{ siteContact.phone_number }}</li>
                     <li>{{ siteContact.email }}</li>
                 </ul>
-                <!-- <div>{{ contact }}</div> -->
             </div>
         </div>
     </MainLayout>
@@ -48,24 +37,12 @@ export default {
     },
     name: "Contact",
     props: {
-        // contact: Object,
         presidentContact: Object,
         siteContact: Object,
     },
     data() {
         return {
             titre: "Contact",
-            // presidente: {
-            //     nom: "Raymonde BELLEC",
-            //     telephone: "06 64 88 60 50",
-            //     email: "raymondebellec@aol.com",
-            // },
-            // adresse: "**À définir**",
-            // contactSite: {
-            //     nom: "Dominique VINCENT",
-            //     telephone: "06 79 58 36 50",
-            //     email: "dominik.vincent@orange.fr",
-            // },
         };
     },
 };
